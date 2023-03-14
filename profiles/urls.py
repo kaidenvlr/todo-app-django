b6e242a04c12a62a7dbdd5ad9796554c8078e71f
kaidenvlr <jzxcvblessed@gmail.com>
2023-03-14 16:44:57 +0600

    delete tasklist url

--- profiles/urls.py
+++ profiles/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
 
     path('create-list', views.create_list_view, name='create-list'),
     path('list/<int:pk>/edit', views.edit_list_view, name='edit-list'),
+    path('list/<int:pk>/delete', views.delete_task_list_view, name='delete-list'),
     path('list/<int:pk>', views.list_view, name='list'),
 
     path('list/<int:pk>/create-task', views.create_task_view, name="create-task"),
