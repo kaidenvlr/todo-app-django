e43020e734fa52e178adfdc5eac90bf847f6c05b
kaidenvlr <jzxcvblessed@gmail.com>
2023-03-16 00:24:55 +0600

    new views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
-# Create your views here.
+from profiles.models import ProfileCategory
+from tasks.forms import TaskForm
+from tasks.models import Task
+
+
+def create_task_view(request, pk):
+    task_list = get_object_or_404(ProfileCategory, pk=pk)
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.task_list = task_list
+            task.save()
+            return redirect('list', pk=pk)
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/create-task.html', {'form': form})
+
+
+def delete_task_view(request, pk, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    task.delete()
+    return redirect('list', pk=pk)
+
+
+def complete_task_view(request, pk, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    task.is_done = True
+    task.save()
+    return redirect('list', pk=pk)
+
+
+def incomplete_task_view(request, pk, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    task.is_done = False
+    task.save()
+    return redirect('list', pk=pk)
