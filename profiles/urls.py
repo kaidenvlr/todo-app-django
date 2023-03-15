05d732508ebc607fb564c14a9e4ed824af78cda4
kaidenvlr <jzxcvblessed@gmail.com>
2023-03-16 00:24:11 +0600

    relocated endpoints to tasks.urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -9,10 +9,7 @@ urlpatterns = [
     path('register', views.register_view, name='register'),
 
     path('create-list', views.create_list_view, name='create-list'),
-    path('list/<int:pk>/edit', views.edit_list_view, name='edit-list'),
-    path('list/<int:pk>/delete', views.delete_task_list_view, name='delete-list'),
-    path('list/<int:pk>', views.list_view, name='list'),
-
-    path('list/<int:pk>/create-task', views.create_task_view, name="create-task"),
-
+    path('list&task=<int:pk>', views.list_view, name='list'),
+    path('list&task=<int:pk>/edit', views.edit_list_view, name='edit-list'),
+    path('list&task=<int:pk>/delete', views.delete_task_list_view, name='delete-list'),
 ]
